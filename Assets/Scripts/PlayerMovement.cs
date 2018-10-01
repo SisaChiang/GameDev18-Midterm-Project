076ea371d2dd943e5f4e2f6e0f415fec5f2d6261
Sisa Chiang <hsc362@nyu.edu>
2018-10-01 13:30:42 -0400

    Scripts

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Usage: put his on a Capsule with a Rigidbody
+// this Script does mouse look and WASD movement for a player
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public float moveSpeed = 1f; 
+	
+	// this variable will remember input and pass it to physics
+	private Vector3 inputVector;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// for mouse look
+
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+		
+		//rotate camera based on mouse input
+
+		transform.Rotate(0f, mouseX, 0f);
+		Camera.main.transform.Rotate(-mouseY, 0f, 0f);
+		
+		// WASD movement
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		
+		// apply keyboard input to position
+		inputVector = transform.forward * vertical;
+		inputVector += transform.right * horizontal;
+
+	}
+
+	void FixedUpdate()
+	{
+		GetComponent<Rigidbody>().velocity = inputVector * moveSpeed + Physics.gravity * 0.5f;
+	}
+	
+}
---
+++ Assets/Scripts/TIme.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// this script is for my countdown timer
+// put this script on a Text display.
+public class TIme : MonoBehaviour
+{
+	private float timeLeft = 300.0f;
+	
+	public Text myTextDisplay;
+
+	private float myScore = 0f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		timeLeft -= Time.deltaTime;
+		myTextDisplay.text = "Time Left: " + Mathf.Round(timeLeft);
+
+		if (timeLeft < 0)
+		{
+			myTextDisplay.text = "Game Over";
+		}
+
+
+
+	}
+}

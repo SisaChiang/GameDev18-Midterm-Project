c0db2c57b2059e3047606ef91a65bd969a12c713
Sisa Chiang <hsc362@nyu.edu>
2018-10-01 23:14:24 -0400

    Obstacles Script attached, obstacles made

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// this script is for win stage, when player collide with the object.
+// that has this script attached on , they get to win stage with a text display.
+// should change to different scene in the future.
 public class Win : MonoBehaviour
 {
 
---
+++ Assets/Scripts/obstacles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// this script is when player collide with obstacles
+// the player object will be destroyed and lead to game over
+// attach this script to the obstacle objects
+
+public class obstacles : MonoBehaviour
+{
+
+	//public Text GameOverText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider activator)
+	{
+		Destroy(activator.gameObject);
+
+		//GameOverText.text = "Game Over!";
+	}
+
+}

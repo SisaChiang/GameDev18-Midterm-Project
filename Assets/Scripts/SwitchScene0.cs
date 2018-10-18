bb1b8e4523d2dbda9ba00eaa07829af1027cb7f9
Sisa Chiang <hsc362@nyu.edu>
2018-10-18 14:17:01 -0400

    Add and Information Scene

---
+++ Assets/Scripts/SwitchScene0.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SwitchScene0 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// apply this on start scene in order to switch to the info scene with space pressed
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			SceneManager.LoadScene("InfoScene");
+		}
+	}
+}
